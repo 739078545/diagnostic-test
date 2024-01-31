25c2b1b487253100ad087fb5fbafa15841d7f6d7
739078545 <lizihan@bu.edu>
2024-01-30 20:12:33 -0500

    modified:   src/globals.ts

--- src/globals.ts
+++ src/globals.ts
@@ -11,7 +11,10 @@ export const TOKEN = "6se7z2q8WGtkxBlXp_YpU-oPq53Av-y_GSYiKyS_COn6AzFuTjj4BQ==";
 export const GET_DEFAULT_HEADERS = () => {
   var headers = new Headers();
   // You will need to add another header here
-  headers.append('TOKEN', TOKEN);
   // If you do not, the API will reject your request (:
-  return headers;
+  const header = new Headers({
+    'Accept': 'application/json',
+    'x-functions-key': TOKEN,
+  });
+  return header;
 };
